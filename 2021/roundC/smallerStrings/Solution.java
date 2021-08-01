670f52b1bc438f3b627bd152c28aa89b90f4d962
santiagen25 <santiago.torrabadella@hotmail.com>
2021-08-01 13:16:11 +0200

    almost got smaller Strings

--- 2021/roundC/smallerStrings/Solution.java
+++ 2021/roundC/smallerStrings/Solution.java
@@ -16,45 +16,35 @@ public class Solution {
 			//Setup
 			
 			//limiteJ redondea para arriba, hacia el numero par (de 3 a 4, de 4 a 4, de 5 a 6, ...)
-			int Nexp = (N + (N % 2));
 			//K^(Nexp/2) son todas las repeticiones posibles que existen
 
 			int interations = (N/2) + (N%2);
-			int[] multiplicadores = new int[interations];
-			int palindromes = 1;
+			int ans = 0;
+			boolean isPalindrome = true;
 			for (int i = 0; i < interations; i++) {
 				char first = S.charAt(i);
 				char last = S.charAt(S.length() - 1 - i);
 
 				char leastChar = 'a';
 
+				if (isPalindrome && first != last) {
+					isPalindrome = false;
+					ans++;
+				}
 				if (first < last) leastChar = first;
 				else leastChar = last;
 				int numericValOfChar = (int) leastChar - 97;
-
-				multiplicadores[i] = numericValOfChar;
-
-				p("numeric value of "+leastChar+" is "+numericValOfChar);
 				
-				int power = 0;
-				int newN = N - (i*2);
-				int exponent = (newN%2) + (newN/2);
+				//Aqui empieza la operacion importante
+				//K^((N/2)+(N%2)-i) * numericValOfChar
+				int exp = interations - i -1;
 
-				if (numericValOfChar > 0) {
-					//p("K = "+K+" and exponent = "+exponent);
-					power = (int) Math.pow(K, exponent);
-				}
-
-				palindromes += power;
-
-				//int leastNum = (Character.getNumericValue(leastChar) - 9);
+				int power = (int) Math.pow(K, exp);
+				ans += (power * numericValOfChar);
 			}
 
-			palindromes--;
-
-
 			//answer
-			p("Case #" + caseId + ": " + palindromes);
+			p("Case #" + caseId + ": " + ans);
 		 }
 	}
 
